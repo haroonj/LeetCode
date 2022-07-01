e0cdcceac488e5abf1982cfc5f23f605f8e09a08
haroonj <jj.haroon99@gmail.com>
2022-07-01 22:19:31 +0300

    Add Two Numbers

--- src/main/java/Medium/AddTwoNumbers.java
+++ src/main/java/Medium/AddTwoNumbers.java
@@ -40,5 +40,25 @@ public class AddTwoNumbers {
             return head.next;
         }
     }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" + "val=" + val + ", next=" + next + '}';
+        }
+    }
+
 }
 
