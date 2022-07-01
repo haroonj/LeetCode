dda75092e57cfc5b15552085b318696dbd117288
haroonj <jj.haroon99@gmail.com>
2022-07-01 22:16:25 +0300

    Add Two Numbers

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Leet Code
 
-This is my Repo for saving my journey solving LeetCode problems.
+### This is my Repo for saving my journey solving LeetCode problems.
 
 ## Easy:
 
 * [Two Sum](https://leetcode.com/problems/two-sum/)
-* [PalindromeNumber](https://leetcode.com/problems/palindrome-number/)
+* [Palindrome Number](https://leetcode.com/problems/palindrome-number/)
 * [Roman To Integer](https://leetcode.com/problems/roman-to-integer/)
 
 ## Medium:
+
+* [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
 
 ## Hard:
--- src/main/java/Medium/AddTwoNumbers.java
+++ src/main/java/Medium/AddTwoNumbers.java
@@ -1,59 +1,43 @@
 package Medium;
 
-import java.math.BigInteger;
 
 //https://leetcode.com/problems/add-two-numbers/
 public class AddTwoNumbers {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        ListNode l0 = new ListNode(9);
+        ListNode l0 = new ListNode(3);
+        ListNode l1 = new ListNode(4, l0);
+        ListNode l2 = new ListNode(2, l1);
 
-        ListNode l10 = new ListNode(9);
-        ListNode l11 = new ListNode(9, l10);
-        ListNode l12 = new ListNode(9, l11);
-        ListNode l13 = new ListNode(9, l12);
-        ListNode l14 = new ListNode(9, l13);
-        ListNode l15 = new ListNode(9, l14);
-        ListNode l16 = new ListNode(9, l15);
-        ListNode l17 = new ListNode(9, l16);
-        ListNode l18 = new ListNode(9, l17);
-        ListNode l19 = new ListNode(1, l18);
-        System.out.println(solution.addTwoNumbers(l0, l19));
+        ListNode l10 = new ListNode(4);
+        ListNode l11 = new ListNode(6, l10);
+        ListNode l12 = new ListNode(5, l11);
+        System.out.println(solution.addTwoNumbers(l2, l12));
     }
 
     static class Solution {
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            BigInteger val1 = toLong(l1);
-            BigInteger val2 = toLong(l2);
-            BigInteger sum = val1.add(val2);
-            if (sum.equals(BigInteger.valueOf(0))) {
-                return new ListNode(0);
-            }
-            ListNode listNode = fromLong(sum);
-            sum = toLong(listNode);
-            return fromLong(sum);
-        }
+            ListNode head = new ListNode(0);
+            ListNode l3 = head;
+            int carry = 0;
+            while (l1 != null || l2 != null) {
+                int l1Val = (l1 != null) ? l1.val : 0;
+                int l2Val = (l2 != null) ? l2.val : 0;
 
-        public BigInteger toLong(ListNode listNode) {
-            BigInteger x = BigInteger.valueOf(0);
-            long rank = 1;
-            while (listNode != null) {
-                x = x.add(BigInteger.valueOf(listNode.val * rank));
-                listNode = listNode.next;
-                rank *= 10;
-            }
-            return x;
-        }
+                int sum = l1Val + l2Val + carry;
+                carry = sum / 10;
 
-        public ListNode fromLong(BigInteger value) {
-            ListNode listNode = null;
-            String val = value.toString();
-            System.out.println(val);
-            for (int i = 0; i < val.length(); i++) {
-                listNode = new ListNode(Integer.parseInt(String.valueOf(val.charAt(i))), listNode);
+                l3.next = new ListNode(sum % 10);
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+                l3 = l3.next;
             }
-            return listNode;
+            if (carry > 0) {
+                l3.next = new ListNode(carry);
+            }
+            return head.next;
         }
     }
 }
