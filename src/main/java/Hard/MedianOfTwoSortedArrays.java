dd253e9c66f59918f3343413e3b9180eb7ac22fd
haroonj <jj.haroon99@gmail.com>
2022-07-10 19:04:39 +0300

    Median of Two Sorted Arrays problem

---
+++ src/main/java/Hard/MedianOfTwoSortedArrays.java
@@ -0,0 +1,15 @@
+package Hard;
+
+//https://leetcode.com/problems/median-of-two-sorted-arrays/
+public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+    }
+
+    static class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            return 0;
+        }
+    }
+}
