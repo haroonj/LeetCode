3b9c8ad80261f2e25d87ea9b7b0ef27fd7890adb
haroonj <jj.haroon99@gmail.com>
2022-07-10 14:51:55 +0300

    Search Insert Position init

---
+++ src/main/java/Easy/SearchInsertPosition.java
@@ -0,0 +1,17 @@
+package Easy;
+
+//https://leetcode.com/problems/search-insert-position/
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 5));
+
+    }
+
+    static class Solution {
+        public int searchInsert(int[] nums, int target) {
+
+            return target;
+        }
+    }
+}
