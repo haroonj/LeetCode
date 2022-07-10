b394bb23d753381b249c7dab5b548d4e6bc43a19
haroonj <jj.haroon99@gmail.com>
2022-07-10 16:07:14 +0300

    Reverse Integer problem

---
+++ src/main/java/Medium/ReverseInteger.java
@@ -0,0 +1,14 @@
+package Medium;
+
+//https://leetcode.com/problems/reverse-integer/
+public class ReverseInteger {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int reverse(int x) {
+            return 0;
+        }
+    }
+}
