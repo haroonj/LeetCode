37d4063c84f27fc54e5a35ab85dee676a310e75c
haroonj <jj.haroon99@gmail.com>
2022-07-10 20:37:33 +0300

    Median of Two Sorted Arrays only odd sum

--- src/main/java/Hard/MedianOfTwoSortedArrays.java
+++ src/main/java/Hard/MedianOfTwoSortedArrays.java
@@ -4,12 +4,24 @@ package Hard;
 public class MedianOfTwoSortedArrays {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+        System.out.println(solution.findMedianSortedArrays(new int[]{1, 4, 6}, new int[]{3, 5}));
     }
 
     static class Solution {
         public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            return 0;
+            int m1 = 0, m2 = 0, index1 = 0, index2 = 0;
+            boolean odd = (nums1.length + nums2.length) % 2 != 0;
+            int divPoint = odd ? (nums1.length + nums2.length) / 2 + 1 : (nums1.length + nums2.length) / 2;
+            for (int i = 0; i < divPoint; i++) {
+                if (nums1[index1] < nums2[index2]) {
+                    m1 = nums1[index1];
+                    index1++;
+                } else if (nums1[index1] >= nums2[index2]) {
+                    m1 = nums2[index2];
+                    index2++;
+                }
+            }
+            return m1;
         }
     }
 }
