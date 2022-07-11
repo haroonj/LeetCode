03492a3d4832cc9ed123f6e0d9f47032fcecc6f1
haroonj <jj.haroon99@gmail.com>
2022-07-12 02:57:28 +0300

    Running Sum of 1d Array sol with 100%

--- src/main/java/Easy/RunningSumOf1DArray.java
+++ src/main/java/Easy/RunningSumOf1DArray.java
@@ -6,12 +6,18 @@ import java.util.Arrays;
 public class RunningSumOf1DArray {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.runningSum(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(solution.runningSum(new int[]{3, 1, 2, 10, 1})));
     }
 
     static class Solution {
         public int[] runningSum(int[] nums) {
-            return nums;
+            int[] sums = new int[nums.length];
+            int prevSum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                prevSum = nums[i] + prevSum;
+                sums[i] = prevSum;
+            }
+            return sums;
         }
     }
 }
