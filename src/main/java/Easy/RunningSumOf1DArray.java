b80cad4cf702e4857cd08eeb2ca57dee73584086
haroonj <jj.haroon99@gmail.com>
2022-07-12 02:52:42 +0300

    Running Sum of 1d Array

--- src/main/java/Easy/RunningSumOf1DArray.java
+++ src/main/java/Easy/RunningSumOf1DArray.java
@@ -1,6 +1,17 @@
 package Easy;
 
+import java.util.Arrays;
+
 //https://leetcode.com/problems/running-sum-of-1d-array/
 public class RunningSumOf1DArray {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.runningSum(new int[]{1, 2, 3, 4})));
+    }
 
+    static class Solution {
+        public int[] runningSum(int[] nums) {
+            return nums;
+        }
+    }
 }
