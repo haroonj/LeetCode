e912a50846fc3600e9efa0b1e912fa1f1901c1a4
haroonj <jj.haroon99@gmail.com>
2022-07-12 06:27:36 +0300

    Is Subsequence sol with 93.01%

--- src/main/java/Easy/IsSubsequence.java
+++ src/main/java/Easy/IsSubsequence.java
@@ -4,12 +4,21 @@ package Easy;
 public class IsSubsequence {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isSubsequence("paper", "title"));
+        System.out.println(solution.isSubsequence("ace", "abcde"));
     }
 
     static class Solution {
         public boolean isSubsequence(String s, String t) {
-            return true;
+            if (s.length() > t.length()) return false;
+            int subIndex = 0;
+            for (int i = 0; i < t.length(); i++) {
+                if (t.length() - i < s.length() - subIndex) {
+                    return false;
+                }
+                if (subIndex == s.length()) return true;
+                if (s.charAt(subIndex) == t.charAt(i)) subIndex++;
+            }
+            return subIndex == s.length();
         }
     }
 }
