eef241340dcf19a5e2490e639465c94119127290
haroonj <jj.haroon99@gmail.com>
2022-07-12 03:00:51 +0300

    Find Pivot Index prob

---
+++ src/main/java/Easy/FindPivotIndex.java
@@ -0,0 +1,15 @@
+package Easy;
+
+//https://leetcode.com/problems/find-pivot-index/
+public class FindPivotIndex {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+    }
+
+    static class Solution {
+        public int pivotIndex(int[] nums) {
+            return 0;
+        }
+    }
+}
