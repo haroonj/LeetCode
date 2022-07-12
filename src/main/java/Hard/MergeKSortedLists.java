0249cf3509864bb16d2be17318d726f8cddd3ff2
haroonj <jj.haroon99@gmail.com>
2022-07-12 17:39:40 +0300

    Merge k Sorted Lists problem

---
+++ src/main/java/Hard/MergeKSortedLists.java
@@ -0,0 +1,18 @@
+package Hard;
+
+
+import util.ListNode;
+
+//https://leetcode.com/problems/merge-k-sorted-lists/
+public class MergeKSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.mergeKLists(new ListNode[]{}));
+    }
+
+    static class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            return null;
+        }
+    }
+}
