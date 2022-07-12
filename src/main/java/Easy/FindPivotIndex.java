21d00068d158cdf07f4b4ed3ef8d6d1e8b78ae2d
haroonj <jj.haroon99@gmail.com>
2022-07-12 03:31:19 +0300

    Find Pivot Index sol with 100%

--- src/main/java/Easy/FindPivotIndex.java
+++ src/main/java/Easy/FindPivotIndex.java
@@ -4,12 +4,20 @@ package Easy;
 public class FindPivotIndex {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+        System.out.println(solution.pivotIndex(new int[]{-1, -1, 0, -1, -1, 0}));
     }
 
     static class Solution {
         public int pivotIndex(int[] nums) {
-            return 0;
+            int rightSum = 0, leftSum = 0;
+            for (int num : nums) rightSum += num;
+
+            for (int i = 0; i < nums.length; i++) {
+                rightSum -= nums[i];
+                if (rightSum == leftSum) return i;
+                leftSum += nums[i];
+            }
+            return -1;
         }
     }
 }
