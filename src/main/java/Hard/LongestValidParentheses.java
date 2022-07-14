a934930df420a3075499a6493e63ae9228e63c73
haroonj <jj.haroon99@gmail.com>
2022-07-14 19:08:16 +0300

    Longest Valid Parentheses problem

---
+++ src/main/java/Hard/LongestValidParentheses.java
@@ -0,0 +1,15 @@
+package Hard;
+
+//https://leetcode.com/problems/longest-valid-parentheses/
+public class LongestValidParentheses {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestValidParentheses(")()())"));
+    }
+
+    static public class Solution {
+        public int longestValidParentheses(String s) {
+            return 0;
+        }
+    }
+}
