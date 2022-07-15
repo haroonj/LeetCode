243a6ea59e74cb097f4f9b3022585c7a8087eb8b
haroonj <jj.haroon99@gmail.com>
2022-07-15 17:07:21 +0300

    Middle of the Linked List sol with 100%

--- src/main/java/Easy/MiddleOfTheLinkedList.java
+++ src/main/java/Easy/MiddleOfTheLinkedList.java
@@ -6,11 +6,27 @@ import util.ListNode;
 public class MiddleOfTheLinkedList {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.middleNode(new ListNode(0)));
+        ListNode lh = new ListNode(6);
+        ListNode l0 = new ListNode(5, lh);
+        ListNode l1 = new ListNode(4, l0);
+        ListNode l2 = new ListNode(3, l1);
+        ListNode l3 = new ListNode(2, l2);
+        ListNode l4 = new ListNode(1, l3);
+        System.out.println(solution.middleNode(l4));
     }
 
     static class Solution {
         public ListNode middleNode(ListNode head) {
+            ListNode dummyHead = new ListNode(0);
+            int numOfNodes = 0;
+            dummyHead.next = head;
+            while (dummyHead.next != null) {
+                numOfNodes++;
+                dummyHead = dummyHead.next;
+            }
+            for (int i = 0; i < numOfNodes / 2; i++) {
+                head = head.next;
+            }
             return head;
         }
     }
