413965adfcd78064c0acb02a25790d74c7707c7f
haroonj <jj.haroon99@gmail.com>
2022-07-16 23:52:19 +0300

    Merge k Sorted Lists sol 90%

--- src/main/java/Hard/MergeKSortedLists.java
+++ src/main/java/Hard/MergeKSortedLists.java
@@ -12,9 +12,31 @@ public class MergeKSortedLists {
 
     static class Solution {
         public ListNode mergeKLists(ListNode[] lists) {
-            ListNode mergedList = new ListNode(0);
+            if(lists.length == 0)
+                return null;
+            int interval = 1;
+            while(interval < lists.length){
+                for (int i = 0; i + interval < lists.length; i = i + interval*2){
+                    lists[i] = mergeTwoLists(lists[i], lists[i+interval]);
+                }
+                interval *= 2;
+            }
 
-            return mergedList.next;
+            return lists[0];
+        }
+
+        private ListNode mergeTwoLists(ListNode l1, ListNode l2){
+            if(l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+            if(l1.val < l2.val){
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            }else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
         }
     }
 }
