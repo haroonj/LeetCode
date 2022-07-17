5c293ac381fe20f716d23e012deb0ce60c9eee15
haroonj <jj.haroon99@gmail.com>
2022-07-17 17:29:57 +0300

    Best Time to Buy and Sell Stock sol with 100%

--- src/main/java/Easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/Easy/BestTimeToBuyAndSellStock.java
@@ -4,12 +4,24 @@ package Easy;
 public class BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.maxProfit(new int[]{}));
+        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
     static class Solution {
         public int maxProfit(int[] prices) {
-            return 0;
+            int lsf = Integer.MAX_VALUE;
+            int op = 0, best;
+
+            for (int price : prices) {
+                if (price < lsf) {
+                    lsf = price;
+                }
+                best = price - lsf;
+                if (op < best) {
+                    op = best;
+                }
+            }
+            return op;
         }
     }
 }
