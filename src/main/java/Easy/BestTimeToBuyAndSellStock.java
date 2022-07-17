4a52d53d9ff537f046e8861b070885f951cee968
haroonj <jj.haroon99@gmail.com>
2022-07-17 17:21:13 +0300

    Best Time to Buy and Sell Stock prob

--- src/main/java/Easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/Easy/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,15 @@
 package Easy;
 
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxProfit(new int[]{}));
+    }
+
+    static class Solution {
+        public int maxProfit(int[] prices) {
+            return 0;
+        }
+    }
 }
