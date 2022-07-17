f192cc2af39c5e61da2ccdce347ac43c7a64192e
haroonj <jj.haroon99@gmail.com>
2022-07-18 00:34:42 +0300

    Permutations problem

---
+++ src/main/java/Medium/Permutations.java
@@ -0,0 +1,16 @@
+package Medium;
+
+import java.util.List;
+
+public class Permutations {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.permute(new int[]{1}));
+    }
+
+    static class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            return null;
+        }
+    }
+}
