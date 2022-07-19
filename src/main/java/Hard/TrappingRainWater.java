764919677784a38cace857f94c1470b268b37dfc
haroonj <jj.haroon99@gmail.com>
2022-07-20 02:37:05 +0300

    Trapping Rain Water sol with 99%

--- src/main/java/Hard/TrappingRainWater.java
+++ src/main/java/Hard/TrappingRainWater.java
@@ -9,7 +9,28 @@ public class TrappingRainWater {
 
     static class Solution {
         public int trap(int[] height) {
-            return 0;
+            int left = 0;
+            int right = height.length - 1;
+            int trappedWater = 0;
+            int maxLeft = 0, maxRight = 0;
+            while (left < right) {
+                if (height[left] < height[right]) {
+                    if (height[left] >= maxLeft) {
+                        maxLeft = height[left];
+                    } else {
+                        trappedWater += maxLeft - height[left];
+                    }
+                    ++left;
+                } else {
+                    if (height[right] >= maxRight) {
+                        maxRight = height[right];
+                    } else {
+                        trappedWater += maxRight - height[right];
+                    }
+                    --right;
+                }
+            }
+            return trappedWater;
         }
     }
 }
