4639b5e5dbbcda261d97ad3dfd0738f3fa7a17a3
haroonj <jj.haroon99@gmail.com>
2022-07-19 21:33:12 +0300

    Longest Substring Without Repeating Characters problem

--- src/main/java/Medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,15 @@
 package Medium;
 
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
 public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    static class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            return 0;
+        }
+    }
 }
