c8c826f4d01eb7d19c3a0670c931b4cf36c84b58
haroonj <jj.haroon99@gmail.com>
2022-07-19 21:59:50 +0300

    Longest Substring Without Repeating Characters sol

--- src/main/java/Medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,15 +1,28 @@
 package Medium;
 
+import java.util.HashSet;
+
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
     }
 
     static class Solution {
         public int lengthOfLongestSubstring(String s) {
-            return 0;
+            HashSet<Character> hashSet = new HashSet<>();
+            int i = 0, j = 0, count = 0;
+            while (j < s.length()) {
+                if (!hashSet.contains(s.charAt(j))) {
+                    hashSet.add(s.charAt(j++));
+                    count = Math.max(count, hashSet.size());
+                } else {
+                    hashSet.remove(s.charAt(i++));
+                }
+            }
+
+            return count;
         }
     }
 }
