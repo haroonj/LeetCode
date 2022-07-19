ff870ab6a4fdcafb5c18e95b01395a58eb6ecb0e
haroonj <jj.haroon99@gmail.com>
2022-07-20 01:50:34 +0300

    Trapping Rain Water problem

--- src/main/java/Hard/TrappingRainWater.java
+++ src/main/java/Hard/TrappingRainWater.java
@@ -1,4 +1,15 @@
 package Hard;
 
+//https://leetcode.com/problems/trapping-rain-water/
 public class TrappingRainWater {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    static class Solution {
+        public int trap(int[] height) {
+            return 0;
+        }
+    }
 }
