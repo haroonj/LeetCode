d4f0e28b64d6749457139cf4da9d247d66fcac45
haroonj <jj.haroon99@gmail.com>
2022-07-20 12:14:40 +0300

    Longest Palindromic Substring prob

--- src/main/java/Medium/LongestPalindromicSubstring.java
+++ src/main/java/Medium/LongestPalindromicSubstring.java
@@ -1,4 +1,14 @@
 package Medium;
-
+//https://leetcode.com/problems/longest-palindromica-substring/
 public class LongestPalindromicSubstring {
+    public static void main(String[] args){
+        Solution solution  = new Solution();
+        System.out.println(solution.longestPalindrome("babad"));
+    }
+    static class Solution {
+
+        public String longestPalindrome(String s) {
+            return s;
+        }
+    }
 }
