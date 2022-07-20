75da79b4bd5ac33afd93aa5d2c7b962dd65c56c8
haroonj <jj.haroon99@gmail.com>
2022-07-20 12:15:17 +0300

    Longest Palindromic Substring sol

--- src/main/java/Medium/LongestPalindromicSubstring.java
+++ src/main/java/Medium/LongestPalindromicSubstring.java
@@ -6,9 +6,28 @@ public class LongestPalindromicSubstring {
         System.out.println(solution.longestPalindrome("babad"));
     }
     static class Solution {
-
+        private int low, maxLen;
         public String longestPalindrome(String s) {
-            return s;
+            int len = s.length();
+            if (len < 2)
+                return s;
+
+            for (int i = 0; i < len-1; i++) {
+                extendPalindrome(s, i, i);
+                extendPalindrome(s, i, i+1);
+            }
+            return s.substring(low, low + maxLen);
+        }
+
+        private void extendPalindrome(String s, int j, int k) {
+            while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+                j--;
+                k++;
+            }
+            if (maxLen < k - j - 1) {
+                low = j + 1;
+                maxLen = k - j - 1;
+            }
         }
     }
 }
