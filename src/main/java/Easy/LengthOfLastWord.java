ece2c4398415bf20f5afefba5d5420457092ad8e
haroonj <jj.haroon99@gmail.com>
2022-07-22 21:58:57 +0300

    Length of Last Word prob

---
+++ src/main/java/Easy/LengthOfLastWord.java
@@ -0,0 +1,14 @@
+package Easy;
+
+//https://leetcode.com/problems/length-of-last-word/
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int lengthOfLastWord(String s) {
+            return 0;
+        }
+    }
+}
