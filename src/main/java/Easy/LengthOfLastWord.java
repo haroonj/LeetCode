ed5dc0cec79836a39d52ff6f8476d179214b25c1
haroonj <jj.haroon99@gmail.com>
2022-07-22 22:26:45 +0300

    Length of Last Word sol with 100%

--- src/main/java/Easy/LengthOfLastWord.java
+++ src/main/java/Easy/LengthOfLastWord.java
@@ -3,12 +3,20 @@ package Easy;
 //https://leetcode.com/problems/length-of-last-word/
 public class LengthOfLastWord {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLastWord(""));
     }
 
     static class Solution {
         public int lengthOfLastWord(String s) {
-            return 0;
+            int length = 0;
+
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) != ' ') length++;
+                else if (length > 0) return length;
+            }
+
+            return length;
         }
     }
 }
