ff4da04f773b33dcce5fa11d86efe098d328b9c6
Haroun <jj.haroon99@gmail.com>
2022-07-23 22:05:00 +0300

    Add Binary problem

--- src/main/java/Easy/AddBinary.java
+++ src/main/java/Easy/AddBinary.java
@@ -1,4 +1,17 @@
 package Easy;
 
+//https://leetcode.com/problems/add-binary/
 public class AddBinary {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.addBinary("10", "1"));
+    }
+
+    static class Solution {
+        public String addBinary(String a, String b) {
+            String sum = "";
+
+            return sum;
+        }
+    }
 }
