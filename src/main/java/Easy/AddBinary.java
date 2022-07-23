8ed9a76d1d4cb94f6d7e89a436047a56c535cb81
Haroun <jj.haroon99@gmail.com>
2022-07-23 22:25:56 +0300

    Add Binary sol with 100%

--- src/main/java/Easy/AddBinary.java
+++ src/main/java/Easy/AddBinary.java
@@ -9,9 +9,24 @@ public class AddBinary {
 
     static class Solution {
         public String addBinary(String a, String b) {
-            String sum = "";
+            StringBuilder Sum = new StringBuilder();
+            int i = a.length() - 1;
+            int j = b.length() - 1;
+            int carry = 0;
 
-            return sum;
+            while (i >= 0 || j >= 0) {
+                int bitSum = carry;
+                if (i >= 0) bitSum += a.charAt(i) - '0';
+                if (j >= 0) bitSum += b.charAt(j) - '0';
+                Sum.append(bitSum % 2);
+                carry = bitSum / 2;
+                i--;
+                j--;
+            }
+            if (carry != 0) {
+                Sum.append(carry);
+            }
+            return Sum.reverse().toString();
         }
     }
 }
