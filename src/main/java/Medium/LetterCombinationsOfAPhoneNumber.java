459b880d25f0ff66b92aeda71b427aeb728ee4d0
Haroun <jj.haroon99@gmail.com>
2022-07-24 20:45:12 +0300

    Letter Combinations of a Phone Number prblem

---
+++ src/main/java/Medium/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,17 @@
+package Medium;
+
+import java.util.List;
+
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+public class LetterCombinationsOfAPhoneNumber {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.letterCombinations("23"));
+    }
+
+    static class Solution {
+        public List<String> letterCombinations(String digits) {
+            return null;
+        }
+    }
+}
