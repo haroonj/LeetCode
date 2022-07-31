42c86f618a427ce954f1daf21895b8e514ee2934
Haroun <jj.haroon99@gmail.com>
2022-07-31 22:59:42 +0300

    Single Number problem

---
+++ src/main/java/Easy/SingleNumber.java
@@ -0,0 +1,15 @@
+package Easy;
+
+//https://leetcode.com/problems/single-number/
+public class SingleNumber {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.singleNumber(new int[]{}));
+    }
+
+    static class Solution {
+        public int singleNumber(int[] nums) {
+            return 0;
+        }
+    }
+}
