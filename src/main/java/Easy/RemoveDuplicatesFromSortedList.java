0fbc153f0304c756de72d054c5e02d8a36210857
Haroun <jj.haroon99@gmail.com>
2022-10-07 14:43:17 +0300

    Remove Duplicates from Sorted List problem

---
+++ src/main/java/Easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,7 @@
+package Easy;
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args){
+
+    }
+}
